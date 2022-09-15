aac120bbfb88251f913636520152271c58464f2b
HasamKjp <91549628+HasamKjp@users.noreply.github.com>
2022-09-15 18:21:24 +0300

    Update script_css.js

--- script_css.js
+++ script_css.js
@@ -2,17 +2,16 @@
       var i=0;
       var imagees =[];
       var time =1700;
-      imagees [0]='pictur/eef.png';
-      imagees [1]='pictur/dssd.jfif';
-      imagees [2]='pictur/dsdsfs.png';
-      imagees [3]='pictur/dfgg.jfif';
+      imagees [0]='dsdsfs.png';
+      imagees [1]='eef.png';
+      imagees [2]='dssd.jfif';
+      imagees [3]='eef.png';
        
 
       function changeImg (){
       document.slide.src=imagees[i];
       if (i<imagees.length-1){
           i++;
-          
       }
       else {
           i=0;
