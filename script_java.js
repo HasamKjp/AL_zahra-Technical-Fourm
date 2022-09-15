6c0a2276398822a40d77c987503238778a06ed14
HasamKjp <91549628+HasamKjp@users.noreply.github.com>
2022-09-15 21:07:47 +0300

    Add files via upload

---
+++ script_java.js
@@ -0,0 +1,24 @@
+      
+      var i=0;
+      var imagees =[];
+      var time =1700;
+      imagees [0]=' download).png';
+      imagees [1]=' javaa.jpg';
+      imagees [2]=' im).jpg';
+      imagees [3]=' dow).png';
+       
+
+      function changeImg (){
+      document.slide.src=imagees[i];
+      if (i<imagees.length-1){
+          i++;
+          
+      }
+      else {
+          i=0;
+      }
+      setTimeout("changeImg()",time);
+      }
+      window.onload=changeImg;
+       
+   
